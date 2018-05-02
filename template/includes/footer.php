3c8df9f34159a6772e92b6be8a565dbbb17a9b96
19stesha69 <33402505+19stesha69@users.noreply.github.com>
2018-05-02 13:07:03 -0700

    Add files via upload

---
+++ template/includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+File Name: footer.php
+Date: 5/2/18
+Programmer: Stesha Norstrom
+*/
+
+echo '<footer>
+    
+        <h1>footer</h1>
+        
+      </footer>';
+
+?>
---
+++ template/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+File Name: header.php
+Date: 5/2/17
+Programmer: Stesha Norstrom
+*/
+
+echo '<header class="headerNav">
+        
+        <input class="hamburger-menu" type="checkbox" id="hamburger-menu" />
+        <label class="menu-icon" for="hamburger-menu"><span class="navicon2"></span></label>
+        
+        <h1 id="subject">Heading (h1)</h1>
+        
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Commercial Website</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Customized WordPress Theme</a></li>
+            <li><a href="#">Animation</a></li>
+        </ul>
+         
+      </header>';
+
+?>
