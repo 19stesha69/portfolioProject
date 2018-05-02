13d2ac96004b973f3710cd55f48d1495655f5819
19stesha69 <33402505+19stesha69@users.noreply.github.com>
2018-05-02 13:04:52 -0700

    Add files via upload

---
+++ template/portfolioTemplate.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+
+<!--
+File Name: portfolio_index.html
+Date: 04/30/18
+Programmer: Stesha Norstrom
+-->
+
+<html>
+    
+<head>
+    
+<title>Stesha Norstrom | Portfolio Template</title>
+    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+<meta name="author" content="Stesha Norstrom">
+    
+<link href="https://fonts.googleapis.com/css?family=Tajawal:400,700" rel="stylesheet">
+    
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/grid.css" rel="stylesheet" type="text/css">  
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+<link href="css/navigation.css" rel="stylesheet" type="text/css">
+    
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/less.js/1.3.1/less.min.js"></script>
+    
+</head>   
+<html>
+<body>
+    
+    <?php include "includes/header.php";?>
+
+    <main>
+        
+        <section id="introBlock">
+            <h1 id="introText">Introductory Text</h1>
+        </section>
+
+        <div class="section group">
+            
+                
+                <article class="col span_1_of_2 span_2_of_2 article_placeholder">
+                    <h1>Article</h1>
+                </article>
+        
+                <article class="col span_1_of_2 span_2_of_2 article_placeholder">
+                    <h1>Article</h1>
+                </article>
+                
+            
+        </div>
+        
+        <div class="section group">
+            
+                
+                <article class="col span_1_of_2 span_2_of_2 article_placeholder">
+                    <h1>Article</h1>
+                </article>
+                
+                <article class="col span_1_of_2 span_2_of_2 article_placeholder">
+                    <h1>Article</h1>
+                </article>
+                
+            </div>
+        
+        
+        <section>
+        <div class="span_2_of_2">
+            <form id="form_placeholder">Form</form>    
+        </div>
+        
+        </section>
+        
+    
+    </main>   
+
+    <?php include "includes/footer.php";?>
+    
+</body>
+</html>
